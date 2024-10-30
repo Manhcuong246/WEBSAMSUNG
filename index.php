8021ed2c0a0619a79a97f3b759bc52d0aec13185
Manhcuong246 <cuongnmph52517@gmail.com>
2024-10-30 23:52:17 +0700

    index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+require_once 'commons/function.php';
+require_once 'controller/homeController.php';
+require_once 'models/homeModel.php';
+
+$act = $_GET['act'] ?? '/';
+match ($act) {
+    '/' => (new homeController())->home(),
+    'signup' => (new homeController())->signup(),
+    'login' => (new homeController())->login(),
+    'detail' => (new homeController())->detail($_GET['id']),
+    'thongbao' => (new homeController())->thongbao(),
+};
+?>
