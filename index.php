527205613b9bdadc58b1724940258d0001c2ac36
minhnc2843 <nguyencongminh0308@gmail.com>
2024-12-10 23:13:48 +0700

    router

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
+session_start();ob_start();
+
 require_once 'commons/function.php';
 require_once 'controller/homeController.php';
+require_once 'controller/AdminController.php';
 require_once 'model/homeModel.php';
 
 $act = $_GET['act'] ?? '/';
@@ -9,5 +12,18 @@ match ($act) {
     'login' => (new homeController())->login(),
     'signup' => (new homeController())->signup(),
      'chitietsp' => (new homeController())->chitietsp($_GET['id']),
+     'logout' => (new homeController())->logout(),
+     'admin' => (new AdminController())->admin(),
+     'productadmin' => (new AdminController())->productadmin(),
+     'profileadmin' => (new AdminController())->profileadmin($_GET['id']),
+     'addproduct' => (new AdminController())->addproduct(),
+     'editsp' => (new AdminController())->editsp($_GET['id']),
+     'xoasp' => (new AdminController())->xoasp($_GET['id']),
+     'giaithichuudai' => (new AdminController())->giaithichuudai(),
+     'giohang' => (new AdminController())->giohang($_SESSION['id']),
+     'xoagiohang' => (new AdminController())->xoagiohang($_GET['id']),
+     'xoabinhluan' => (new AdminController())->xoabinhluan($_GET['id'],$_GET['idsp']),
+     'themdanhmuc' => (new AdminController())->themdanhmuc(),
+    
 };
 ?>
